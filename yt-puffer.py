50e9094bad2c3046aaa6df2bcdfd473237ca8f27
dskrad <dskrad@me.com>
2014-05-19 15:12:52 -0400

    Updates yt-puffer.py to use a JSON config file (puff.json) synced with Dropbox
    The JSON file must have a "state": 0 which allows downloading
    and huffduffing of the "id": "youtube-id". After processing,
    "state": is changed to 1, and "title" is updated.

--- yt-puffer.py
+++ yt-puffer.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python
-import os
+import os, json
 from subprocess import check_output
 from pushbullet import note
 
-puffer_path = "/media/backup/Dropbox/Apps/PlainText 2/Puffer.txt"
+fp = "/media/backup/Dropbox/Apps/PlainText 2/puff.json"
 mp3_path = "/symserv/audio/"
 args = ["youtube-dl","-x", "-t", "--audio-format=mp3"]
-
-if os.path.exists(puffer_path):
-  ytid = open(puffer_path, 'r').read().strip()
-  args.append(ytid)
+with open(fp) as fo:
+  js = json.load(fo)
+print js
+if js['state'] == 0:
+  args.append(js['id'])
   os.chdir(mp3_path)
   result = check_output(args)
   for f in os.listdir("."):
-    if ytid in f:
-      note("yt-puffer", result)
+    if js['id'] in f:
+      note("youtube-dl", result)
       url = "http://d-k.mooo.com/Roku/Audio/" + f
       title, descr = "-".join(f.split("-")[:-1]), ""
+      js['title'] = title
       result = check_output(["puff.py", url, title, descr])
       note("Puff.py", result)
-  with open(puffer_path, 'w') as fn:
-    fn.close()
+  js['state'] = 1
+  with open(fp,"w") as fw:
+    json.dump(js, fw, separators=(",\n", ": "))
