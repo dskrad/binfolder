b46861d535c4a8f9ace245e477becbaffd8c832a
dskrad <dskrad@me.com>
2014-05-22 23:11:31 -0400

    Updates pushbullet.py to make it more versatile

--- pushbullet.py
+++ pushbullet.py
@@ -3,32 +3,36 @@ from urllib import urlopen, urlencode
 import sys, os
 key = os.environ['PUSHBULLET_APIKEY']
 device = os.environ['PUSHBULLET_IPHONE']
-url = "https://%s:@api.pushbullet.com/api/pushes" % key
+push_url = "https://%s:@api.pushbullet.com/api/pushes" % key
 
-def note(title, body):
+def note(*args):
+  title, body = args[0], ""
+  if len(args) == 2:
+    body = args[1]
   params = urlencode({
-    "device_iden": device,
+    #"device_iden": device,
     "type": "note",
     "title": title,
     "body": body
     })
-  urlopen(url, params)
+  urlopen(push_url, params)
 
-def link(title, url):
+def link(*args):
+  title, url = args
   params = urlencode({
-    "device_iden": device,
+    #"device_iden": device,
     "type": "link",
     "title": title,
     "url": url
     })
-  urlopen(url, params)
+  urlopen(push_url, params)
 
 def main():
-  ntype, title, url_body = sys.argv[1:]
+  ntype, args = sys.argv[1], sys.argv[2:]
   if ntype == "note":
-    note(title, url_body)
+    note(*args)
   if ntype == "link":
-    link(title, url_body)
+    link(*args)
 
 if __name__ == "__main__":
   main()
